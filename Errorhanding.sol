e6c8226e1421e19fbef6f853fde2f43ace21b499
Tanu Pal <tanup0010@gmail.com>
2024-06-22 01:34:28 +0530

    Update Errorhanding.sol

--- Errorhanding.sol
+++ Errorhanding.sol
@@ -1,39 +1,40 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
-pragma solidity 0.8.0;
+contract GradingSystem {
+    enum Grade { A, B, C ,D,E,F}
 
-contract AutomaticShop {
+    mapping(address => Grade) public grades;
 
-    uint public catalogue=0;
-    uint public Bill=0;
-    uint public balance=0;
-    bool insideshop=false;
-    
-    function enterShop() public {
-         catalogue=0;
-         insideshop=true;
-         if(Bill !=0){
-         revert("pay prev Bill");
+    event GradeAssigned(address student, Grade grade);
+
+    function assignGrade(address student, uint8 marks) public {
+       
+        require(marks >= 0 && marks <= 32, "Invalid marks. Marks should be between 0 and 32");
+
+       
+        uint8 gradeValue = marks / 15;  
+
+        assert(gradeValue >= 0 && gradeValue <= 5);
+
+        Grade grade;
+        if (gradeValue == 5) {
+            grade = Grade.A;
+        } else if (gradeValue == 4) {
+            grade = Grade.B;
+        } else if (gradeValue == 3) {
+            grade = Grade.C;
+        } else if (gradeValue == 2) {
+            grade = Grade.D;
+        } else if (gradeValue == 1) {
+            grade = Grade.E;
+        }else if (gradeValue == 0) {
+            grade = Grade.F;
+        }else {
+            revert("Invalid grade.");
         }
-    }
-    function addItem() public {
-        catalogue++;
-    }
-    function payBill() public {
-        require(Bill>0,"no bill left");
-        require(Bill<balance,"low balance");
-            balance -= Bill;
-            Bill=0;
-    }
-    function exitShop() public {
-        assert(insideshop);
-            Bill= catalogue*40;
-            catalogue=0;
-            insideshop=false;
-    }
-    function addMoney(uint amount) public{
-         balance += amount;
-    }
+        grades[student] = grade;
+        emit GradeAssigned(student, grade);
+    }
 }
-
 
